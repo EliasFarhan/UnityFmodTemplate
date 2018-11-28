a325ef0bd94a56efe2a37ef619dd3876b3a32d4f
efarhan <elias.farhan@gmail.com>
2018-11-28 14:38:16 +0100

    Adding all assets and git ignore

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+	[FMODUnity.EventRef] [SerializeField] private string musicEvent;
+	
+	FMOD.Studio.EventInstance musicInstance;
+
+	private float timer = 0.0f;
+	// Use this for initialization
+	void Start () 
+	{
+	//	FMODUnity.RuntimeManager.PlayOneShot(musicEvent);
+		musicInstance = FMODUnity.RuntimeManager.CreateInstance(musicEvent);
+		musicInstance.start();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		timer += Time.deltaTime;
+		musicInstance.setParameterValue("MusicTransition", (timer / 5.0f) % 2.0f);
+	}
+}
